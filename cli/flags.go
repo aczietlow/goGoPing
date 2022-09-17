352735d8585a9171d275313b990bebf3aa3d4491
aczietlow <aczietlow@gmail.com>
2022-09-16 20:04:23 -0400

    Leaving love letter for future Chris.

--- cli/flags.go
+++ cli/flags.go
@@ -23,6 +23,7 @@ func initFlags() arguments {
 	var flagFloat flagFloat32
 	count := flag.Int("c", 2, "Stop after sending (and receiving) Count ECHO_RESPONSE packets.  If this option is not specified, ping will operate until interrupted.")
 	// @TODO figure out if the super users part is enforced at the OS level or within Ping. & do that -
+	// @TODO defining a custom flag var was a little extra for the actual needs here. Especially when we convert it to a floag64 later. Leaving this here for a hot minute as a lesson in interfaces and self-inflected pain.
 	flag.Var(&flagFloat, "w", "Wait interval seconds between sending each packet. The default is to wait for one second between each packet normally, or not to wait in flood mode. Only super-user may set interval to values less 0.2 seconds.")
 	// Called to parse all the flags.
 	flag.Parse()
